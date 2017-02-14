327d256aaa9264695cce60ae722bfb02e6121e8f
jose roldan <josebroldant@gmail.com>
2017-02-14 08:37:35 -0500

    se agrego el archivo test.c

---
+++ test.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+printf("Inserte un numero y le daremos la mitad");
+int variable, variable_res;
+scanf("%d%*c", &variable);
+variable_res=variable/2;
+printf("El resultado es: [%d]\n", variable_res);
+}
